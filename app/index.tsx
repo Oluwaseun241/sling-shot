bfe84bea99027b880fd9129b6aa8816f8f1b74f0
Oluwaseun241 <tanimolaoluwaseun70@gmail.com>
2024-10-02 21:22:47 +0100

    wip: pofff

--- app/index.tsx
+++ app/index.tsx
@@ -1,35 +1,67 @@
+import { Ionicons } from "@expo/vector-icons";
+import { Image, StyleSheet, View, SafeAreaView, Text } from "react-native";
 import {
-  Image,
-  StyleSheet,
-  View,
-  Platform,
-  SafeAreaView,
-  Text,
-} from "react-native";
+  PanGestureHandler,
+  PanGestureHandlerGestureEvent,
+} from "react-native-gesture-handler";
+import Animated, {
+  useAnimatedGestureHandler,
+  useAnimatedStyle,
+  useSharedValue,
+} from "react-native-reanimated";
 
 export default function HomeScreen() {
+  const translateX = useSharedValue(0);
+  const translateY = useSharedValue(0);
+  const panGesture = useAnimatedGestureHandler<PanGestureHandlerGestureEvent>({
+    onActive: (event) => {
+      translateY.value = event.translationY;
+      // Unable to archieve the curvy(bow) text with the gesture
+      translateX.value = -Math.pow(event.translationY / 100, 2);
+    },
+    onEnd: () => {
+      translateX.value = 0;
+      translateY.value = 0;
+    },
+  });
+  const rstyle = useAnimatedStyle(() => ({
+    transform: [
+      { translateX: translateX.value },
+      { translateY: translateY.value },
+    ],
+  }));
   return (
-    <SafeAreaView>
-      <Text>Heyy</Text>
-    </SafeAreaView>
+    <Animated.View>
+      <SafeAreaView>
+        <View style={styles.header}>
+          <Ionicons name="person-circle" size={54} color="black" />
+        </View>
+        <PanGestureHandler onGestureEvent={panGesture}>
+          <Animated.View style={[styles.body, rstyle]}>
+            <Text style={{ fontSize: 30, fontWeight: "bold" }}>$4.50</Text>
+            <Text
+              style={{
+                marginTop: "10%",
+                fontSize: 24,
+                fontWeight: "600",
+                letterSpacing: 8,
+              }}
+            >
+              SWIPE DOWN
+            </Text>
+          </Animated.View>
+        </PanGestureHandler>
+      </SafeAreaView>
+    </Animated.View>
   );
 }
 
 const styles = StyleSheet.create({
-  titleContainer: {
-    flexDirection: "row",
+  header: {
     alignItems: "center",
-    gap: 8,
   },
-  stepContainer: {
-    gap: 8,
-    marginBottom: 8,
-  },
-  reactLogo: {
-    height: 178,
-    width: 290,
-    bottom: 0,
-    left: 0,
-    position: "absolute",
+  body: {
+    alignItems: "center",
+    marginTop: "55%",
   },
 });
