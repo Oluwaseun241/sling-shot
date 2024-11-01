8d9fb6824f232e2cc4b4d35fbea01ff6921332c0
Oluwaseun241 <tanimolaoluwaseun70@gmail.com>
2024-11-01 13:22:28 +0100

    wip

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -4,6 +4,7 @@ import * as SplashScreen from "expo-splash-screen";
 import { useEffect } from "react";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
 import "react-native-reanimated";
+import { LinearGradient } from "expo-linear-gradient";
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync();
@@ -25,9 +26,14 @@ export default function RootLayout() {
 
   return (
     <GestureHandlerRootView>
-      <Stack>
-        <Stack.Screen name="index" options={{ headerShown: false }} />
-      </Stack>
+      <LinearGradient
+        colors={["rgba(0,0,0,0.8)", "transparent"]}
+        style={{ backgroundColor: "orange" }}
+      >
+        <Stack>
+          <Stack.Screen name="index" options={{ headerShown: false }} />
+        </Stack>
+      </LinearGradient>
     </GestureHandlerRootView>
   );
 }
